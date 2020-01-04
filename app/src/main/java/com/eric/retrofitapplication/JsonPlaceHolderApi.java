848460ebe6f31e10a9144de12bdba762027b7f8b
Eric Alencar <eric_tonarede@hotmail.com>
2020-01-03 21:41:48 -0300

    criação da classe posts e configuração da listagem dos dados da API na view

---
+++ app/src/main/java/com/eric/retrofitapplication/JsonPlaceHolderApi.java
@@ -0,0 +1,10 @@
+package com.eric.retrofitapplication;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
